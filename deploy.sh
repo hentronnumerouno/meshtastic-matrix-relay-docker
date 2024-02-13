bad201be1445169737da8967e1c4420abaea475e
henry rendler <henry@hwrrobotics.com>
2024-02-13 03:01:32 -0800

    pulls 0.5.2 release

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #docker system prune -f
+git clone https://github.com/geoffwhittington/meshtastic-matrix-relay.git
+cd meshtastic-matrix-relay
+git checkout v0.5.2
+
 docker rm meshtastic-matrix-relay-docker
 ./build.sh
 docker-compose up -d
